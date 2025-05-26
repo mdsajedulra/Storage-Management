2bab2ec3daff757298c037c1c47113d832038e6b
Md Sajedul Islam <mdsajedulra@gmail.com>
2025-05-26 21:43:19 +0600

    test

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import config from "./app/config";
 async function server() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
+    app.listen(config.port || 5000, () => {
       console.log(`Server running on port ${config.port}`);
     });
   } catch (error) {
